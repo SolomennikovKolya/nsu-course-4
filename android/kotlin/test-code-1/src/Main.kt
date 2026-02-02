98d23db1e65545944f28d0bfe7df60d9aee8906d
Nikvel <52555203+SolomennikovKolya@users.noreply.github.com>
2026-02-02 14:59:07 +0700

    kotlin + dimloma docs

---
+++ android/kotlin/test-code-1/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var firstName = "Tom"
+    var lastName = "Smith"
+    var welcome = "Hello, $firstName $lastName"
+    println(welcome)
+
+    lastName = "Smith1"
+    welcome = "Hello, $firstName $lastName"
+    println(welcome)
+}
